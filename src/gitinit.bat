888cdc7e689bbbd8c6da8061bbab79e54e3e37e7
pchuan98 <haeer98@outlook.com>
2023-11-15 17:06:07 +0800

    feat: more human func and output

--- src/gitinit.bat
+++ src/gitinit.bat
@@ -11,45 +11,87 @@
 
 @echo off
 
-IF EXIST .git (
-    echo.
-    call "%~dp0colors.bat" 2 0 "This folder is already a git repository."
-    exit /b 1
-)
-
 :: detected the npm enviroment
 call npm -v >nul 2>&1
 IF ERRORLEVEL 1 (
-    echo.
-    call "%~dp0colors.bat" 2 0 " npm is not installed. Please download and install it first."
-    echo.
-    call echo  - https://nodejs.org/en/download
+    call "%~dp0colors.bat" 2 0 "[E] Npm is not installed. Please download and install it first. The download url : https://nodejs.org/en/download/"
     exit /b 1
 )
 
-call npm ls -g --silent commitizen > nul || call "%~dp0colors.bat" 4 0 "Installing commitizen ..." && call npm install --silent -g commitizen
-call npm ls -g --silent cz-conventional-changelog > nul || call "%~dp0colors.bat" 4 0 "Installing cz-conventional-changelog ..." && call npm install --silent -g cz-conventional-changelog
+call npm ls -g --silent commitizen > nul || call "%~dp0colors.bat" 4 0 "[I] Installing commitizen ..." ^
+    && call npm install --silent -g commitizen
+call npm ls -g --silent cz-conventional-changelog > nul || call "%~dp0colors.bat" 4 0 "[I] Installing cz-conventional-changelog ..." ^
+    && call npm install --silent -g cz-conventional-changelog
 
-echo {"path": "cz-conventional-changelog"} > .czrc
-echo .czrc > .gitignore
-type "%~dp0templates\ignore\vs" >> .gitignore
-type "%~dp0templates\license\mit" >> LICENSE
-
-mkdir src
-mkdir docs
-mkdir test
-mkdir build
-mkdir benchmark
-mkdir imgs
-mkdir .github
-
-type nul > README.md
-
-call git init >nul 2>&1
-
-IF ERRORLEVEL 1 (
-    echo.
-    call "%~dp0colors.bat" 2 0 "Failed to initialize git repository."
-    exit /b 1
+: append .czrc for cz-conventional-changelog
+IF NOT EXIST .czrc (
+    echo {"path": "cz-conventional-changelog"} > .czrc
+    call "%~dp0colors.bat" 3 0 "[I] Append .czrc file"
+) ELSE (
+    call "%~dp0colors.bat" 4 0 "[W] The .czrc file already exists"
 )
-call "%~dp0colors.bat" 3 0 "Initialized git repository."
+
+: append .czrc to .gitignore
+IF NOT EXIST .gitignore (
+    echo .czrc> .gitignore
+    type "%~dp0..\templates\ignore\vs" >> .gitignore
+    call "%~dp0colors.bat" 3 0 "[I] .gitignore file created"
+) ELSE (
+    setlocal Enabledelayedexpansion
+        set /p firstLine=<.gitignore
+        if "!firstLine!" neq ".czrc" (
+            call "%~dp0colors.bat" 4 0 "[W] Append .czrc to .gitignore"
+            echo .czrc> temp
+            type .gitignore >> temp
+            move /y temp .gitignore > nul
+        ) ELSE (
+            call "%~dp0colors.bat" 4 0 "[W] The .gitignore file already exists"
+        )
+    endlocal
+)
+
+: append LICENSE
+IF NOT EXIST LICENSE (
+    type "%~dp0..\templates\license\mit" >> LICENSE
+    call "%~dp0colors.bat" 3 0 "[I] LICENSE file created"
+) ELSE (
+    call "%~dp0colors.bat" 4 0 "[W] The LICENSE file already exists"
+)
+
+: valid and append
+IF NOT EXIST src ( mkdir src && call "%~dp0colors.bat" 3 0 "[I] The src folder created" ) ^
+    ELSE call "%~dp0colors.bat" 4 0 "[W] The src folder already exists"
+
+IF NOT EXIST docs ( mkdir docs && call "%~dp0colors.bat" 3 0 "[I] The docs folder created" ) ^
+    ELSE call "%~dp0colors.bat" 4 0 "[W] The docs folder already exists"
+
+IF NOT EXIST test ( mkdir test && call "%~dp0colors.bat" 3 0 "[I] The test folder created" ) ^
+    ELSE call "%~dp0colors.bat" 4 0 "[W] The test folder already exists"
+
+IF NOT EXIST build ( mkdir build && call "%~dp0colors.bat" 3 0 "[I] The build folder created" ) ^
+    ELSE call "%~dp0colors.bat" 4 0 "[W] The build folder already exists"
+
+IF NOT EXIST benchmark ( mkdir benchmark && call "%~dp0colors.bat" 3 0 "[I] The benchmark folder created" ) ^
+    ELSE call "%~dp0colors.bat" 4 0 "[W] The benchmark folder already exists"
+
+IF NOT EXIST imgs ( mkdir imgs && call "%~dp0colors.bat" 3 0 "[I] The imgs folder created" ) ^
+    ELSE call "%~dp0colors.bat" 4 0 "[W] The imgs folder already exists"
+
+IF NOT EXIST .github ( mkdir .github && call "%~dp0colors.bat" 3 0 "[I] The .github folder created" ) ^
+    ELSE call "%~dp0colors.bat" 4 0 "[W] The .github folder already exists"
+
+IF NOT EXIST README.md ( type nul > README.md && call "%~dp0colors.bat" 3 0 "[I] The README.md file created" ) ^
+    ELSE call "%~dp0colors.bat" 4 0 "[W] The README.md file already exists"
+
+IF EXIST .git ( call "%~dp0colors.bat" 4 0 "[W] This folder is already a git repository." ) ELSE (
+    call git init >nul 2>&1
+
+    IF ERRORLEVEL 1 (
+        echo.
+        call "%~dp0colors.bat" 2 0 "[E] Failed to initialize git repository."
+        exit /b 1
+    )
+    call "%~dp0colors.bat" 3 0 "[I] Initialized git repository."
+)
+
+call "%~dp0colors.bat" 3 0 "[I] Git repository created successfully."
