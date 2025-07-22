7903455f3ad7885c6b66aef3d25606e1080f4b2d
chuan <haeer98@outlook.com>
2025-07-22 17:01:03 +0800

    feat: update the editor config

---
+++ src/ccc.bat
@@ -0,0 +1,8 @@
+@echo off
+
+set http_proxy=http://127.0.0.1:1080
+set https_proxy=http://127.0.0.1:1080
+
+set ANTHROPIC_BASE_URL="https://one.api4gpt.com/"
+set ANTHROPIC_API_KEY=sk-uULxiXzbI3u0hUCwOU9JpFuoQZewOFZMbNDMvK0lgnowkBua
+claude
