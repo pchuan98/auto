2cb16e1b7459dde067eb8aa4555843703666ca6f
chuan <haeer98@outlook.com>
2025-09-09 12:22:28 +0800

    feat: update some scripts

---
+++ alias/ccc.bat
@@ -0,0 +1,9 @@
+@echo off
+setlocal
+
+set http_proxy=http://127.0.0.1:1080
+set https_proxy=http://127.0.0.1:1080
+
+claude --verbose %*
+
+endlocal
--- src/ccc.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-set http_proxy=http://127.0.0.1:1080
-set https_proxy=http://127.0.0.1:1080
-
-@rem set ANTHROPIC_BASE_URL="https://one.api4gpt.com/"
-@rem set ANTHROPIC_API_KEY=sk-uULxiXzbI3u0hUCwOU9JpFuoQZewOFZMbNDMvK0lgnowkBua
-claude %*
